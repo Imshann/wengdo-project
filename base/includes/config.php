20e2d77b034dac6d12d0363a903c9986301954e5
shann huang <1829823496@qq.com>
2015-09-22 20:14:01 +0800

    0.0.1

---
+++ base/includes/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 配置文件
+ */
+
+// 时区设置
+$timezone = 'Asia/Shanghai';
+
+// 页面显示编码设置
+$encoding = 'utf-8';
+
+// 错误报告等级设置
+$error = -1;
--- base/init.php
+++ base/init.php
@@ -3,18 +3,20 @@
  * 初始化文件
  */
 
-// 设置页面显示编码
-header('Content-Type:text/html;charset=utf-8');
-
-// 时区设置
-date_default_timezone_set('Asia/Shanghai');
-
-// 错误报告
-error_reporting(-1);
-
 // 网站根目录
 define('ROOT_PATH', str_replace('\\', '/', dirname(__FILE__)) . '/');
 
-// 载入includes目录下的文件
+// 载入配置文件
 require ROOT_PATH . 'includes/config.php';
+
+// 设置页面显示编码
+header("Content-Type:text/html;charset={$encoding}");
+
+// 时区设置
+date_default_timezone_set( $timezone );
+
+// 错误报告
+error_reporting( $error );
+
+// 载入自定义函数库
 require ROOT_PATH . 'includes/functions.php';
